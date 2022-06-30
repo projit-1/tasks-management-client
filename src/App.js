f05fc8ced981e40b1c09037418fb2584938b9a3a
projit-1 <projitmondol@gmail.com>
2022-06-30 16:59:57 +0600

    banner image is set

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 import ToDo from './Pages/Shared/Home/ToDo';
 import Completed from './Pages/Shared/Home/Completed';
+import Banner from './Pages/Shared/Banner';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Banner></Banner>
       <Routes>
         <Route path='/' element={<ToDo />}></Route>
         <Route path='/completed' element={<Completed />}></Route>
