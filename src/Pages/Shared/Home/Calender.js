d7f2cc1950d39eddf334989ba435b55a068f8610
projit-1 <projitmondol@gmail.com>
2022-07-01 03:47:28 +0600

    calender is added

--- src/Pages/Shared/Home/Calender.js
+++ src/Pages/Shared/Home/Calender.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 const Calander = () => {
+    const [date, setDate] = useState(new Date());
+
     return (
         <div>
-            <h2>this is calander</h2>
+            <h2 className='text-2xl text-center'>Calander</h2>
+            <div className='flex items-center justify-center'>
+                <DayPicker
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                />
+                <p className='text-xl'>You hane Selected :{format(date, 'PP')}</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Home/ToDo.js
+++ src/Pages/Shared/Home/ToDo.js
@@ -5,14 +5,14 @@ import Task from './Task';
 const ToDo = ({ task }) => {
     const [tasks, setTasks] = useState([])
     useEffect(() => {
-        fetch('tasks.json')
+        fetch('http://localhost:5000/task')
             .then(res => res.json())
             .then(data => setTasks(data))
     }, [])
     return (
         <div>
             <h2 className='text-center text-2xl text-primary'>To-DoTaqsks: {tasks.length}</h2>
-            <div className='grid lg:grid-cols-3 grid-cols-1 gap-5'>
+            <div className='grid lg:grid-cols-2 grid-cols-1 gap-5'>
                 {
                     tasks.map(task => <Task
                         key={task._id}
